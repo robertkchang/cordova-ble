60cc6970efaacbd9b9cedc2ff00ff35819937323
Robert Chang <robertkchang@yahoo.com>
2015-03-12 07:08:57 -0700

    First commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.randdusing.bluetoothle/www/bluetoothle.js",
+        "id": "com.randdusing.bluetoothle.BluetoothLe",
+        "clobbers": [
+            "window.bluetoothle"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.randdusing.bluetoothle": "2.1.0"
+}
+// BOTTOM OF METADATA
+});
